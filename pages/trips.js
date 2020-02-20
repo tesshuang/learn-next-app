979a2ca7bf83644217d810e9f0f35f675e3347a3
Tess Huang <tesshuangxj@yahoo.com>
2020-02-20 15:11:49 -0800

    added some meta data to the trips page

--- pages/trips.js
+++ pages/trips.js
@@ -1,9 +1,16 @@
 import Layout from '../components/layout'
+import Head from 'next/head'
 // import Link from 'next/link'
 
 const Trips = () => {
   return (
     <div>
+    <Head>
+        <title>Trips</title>
+        <meta name="description" content="Want to travel to Canada? Come and browse all our trips picked by the Canadian insider." />
+        <meta name="keywords" content="Canada, Canadian, Travel to Canada" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
       <Layout>
         <h1>This is trip page with all trips</h1>
       </Layout>
