005cd7887bdda68e1789c03734738fae38d7dc18
Tess Huang <tesshuangxj@yahoo.com>
2020-08-17 13:15:15 -0700

    Added account component

---
+++ components/account.js
@@ -0,0 +1,14 @@
+const Account = () => (
+  <div>
+    <button>
+      <img src="" alt="Avatar" />
+    </button>
+    <div>
+      <a href="#">Account settings</a>
+      <a href="#">Support</a>
+      <a href="#">Sign out</a>
+    </div>
+  </div>
+);
+
+export default Account;
