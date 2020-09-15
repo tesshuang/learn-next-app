cf2fd1db5641e25e259766c68c84062b3ee40fa9
Tess Huang <tesshuangxj@yahoo.com>
2020-09-15 14:56:39 -0700

    Added handleEscape func and extra button

--- components/account.js
+++ components/account.js
@@ -1,12 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export const Account = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleEscape = (e) => {
+    if (e.key === 'Esc' || e.key === 'Escape') {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscape);
+
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, [handleEscape]);
+
   return (
     <div>
       <button
-        className="block h-8 w-8 overflow-hidden rounded-full border-2 border-gray-600 focus:outline-none focus:border-white"
+        className="z-10 relative block h-8 w-8 overflow-hidden rounded-full border-2 border-gray-600 focus:outline-none focus:border-white"
         onClick={() => setIsOpen(!isOpen)}
       >
         <img
@@ -15,8 +29,15 @@ export const Account = () => {
           alt="Avatar"
         />
       </button>
+      {isOpen && (
+        <button
+          className="fixed inset-0 h-full w-full bg-black opacity-50 m-0 cursor-default"
+          onClick={() => setIsOpen(false)}
+          tabIndex="-1"
+        ></button>
+      )}
       <div
-        className={`mt-2 py-2 w-48 bg-white rounded-lg shadow-xl ${
+        className={`absolute mt-2 py-2 w-48 bg-white rounded-lg shadow-xl ${
           isOpen ? 'block' : 'hidden'
         }`}
       >
